8393c860c1543d11b0592900274dbf2e0b5e3288
pablojmarti <pablojmarti@gmail.com>
2020-05-31 14:17:34 -0400

    Setup non-depricated assert

--- test/player_test.rb
+++ test/player_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
-describe "Player" do
+describe "CMG::Player" do
   it "has player name" do
     name = "John Doe"
     CMG::Player.new(name)
-    CMG::Player.name.must_equal name
+    _(CMG::Player.name).must_equal name
   end
 end
