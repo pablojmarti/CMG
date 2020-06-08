53727cfee6901ba6b466dafa372de7af0730be22
pablojmarti <pablojmarti@gmail.com>
2020-06-07 20:45:06 -0400

    create game and start initializing

--- lib/CMG/game.rb
+++ lib/CMG/game.rb
@@ -1,14 +1,19 @@
 module CMG
   require 'active_model'
 
-  class Game
+  class Game 
     include ::ActiveModel::Validations
-    attr_reader :player
 
-    validates :player, presence: true
+    attr_reader :player
+    attr_accessor :turns
+
+    validate :player, :presence,
+      unless: -> { player.invalid? }
+    validates :turns, presence: true
 
     def initialize(name)
       @player = CMG::Player.new(name)
+      @turns = 0
     end
   end
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -1,10 +1,21 @@
 require 'test_helper'
 
 describe "CMG::Game" do
+  before do
+    @game = CMG::Game.new("John Doe")
+  end
+
   describe "When A Game Gets initialized" do
     it "must create a player object" do
-      @game = CMG::Game.new("John Doe")
       assert_kind_of CMG::Player, @game.player
+    end
+
+    it "must not be created without a player" do
+      assert CMG::Game.new(nil).player.invalid?
+    end
+
+    it "must create a turn counter set to 0" do
+      assert_equal 0, @game.turns
     end
   end
 end
